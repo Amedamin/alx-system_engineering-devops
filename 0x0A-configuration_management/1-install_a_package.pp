796a05c0698883f198b0f9b70d962e6586578c09
Amedamin <ahmedqryqr@gmail.com>
2024-03-22 04:04:07 -0700

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -14,8 +14,3 @@ package { 'Werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3',
 }
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/usr/bin'],
-}
