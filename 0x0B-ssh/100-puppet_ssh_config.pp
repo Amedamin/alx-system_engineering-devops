5d4b163945cb9deac03242e74cc0f5f737a39d39
Amedamin <ahmedqryqr@gmail.com>
2024-03-23 04:53:17 -0700

    done

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,10 +5,14 @@ file_line { 'Turn off passwd auth':
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
   match  => '^#?\s*PasswordAuthentication',
+  replace            => true,
+  append_on_no_match => true
 }
 
 file_line { 'Declare identity file':
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school',
   match  => '^#?\s*IdentityFile',
+  replace            => true,
+  append_on_no_match => true
 }
